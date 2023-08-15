6cd59e664ebdf34c22694c82f70343fea8617ba5
italoprovensi <italoprovensi777@gmail.com>
2023-08-14 21:36:42 -0300

    primeiros passos

---
+++ pythonProject/calcular_almento_de_salario.py
@@ -0,0 +1,27 @@
+salario_original = float(input("seu salario: "))
+salario_desconto = salario_original
+
+if salario_original <= 280.00:
+    salario_desconto += salario_desconto * 0.2
+    porcentagem = "20%"
+    aumento = salario_original * 0.20
+
+elif salario_original <= 700.00:
+    salario_desconto += salario_desconto * 0.15
+    porcentagem = "15%"
+    aumento = salario_original * 0.15
+
+elif salario_original <= 1500.00:
+    salario_desconto += salario_desconto * 0.10
+    porcentagem = "10%"
+    aumento = salario_original * 0.10
+
+elif salario_original > 1500.00:
+    salario_desconto += salario_desconto * 0.05
+    porcentagem = "5%"
+    aumento = salario_original * 0.05
+
+print("O salário antes do reajuste:", salario_original)
+print("O percentual de aumento aplicado",porcentagem)
+print("O valor do aumento",aumento)
+print("novo salário, após o aumento",salario_desconto)
---
+++ pythonProject/calcular_nota.py
@@ -0,0 +1,29 @@
+nota_1 =float(input("insira sua primeira nota de 0.00 a 10.00:"))
+nota_2 =float(input("insira sua segunda nota de 0.00 a 10.00:"))
+soma = nota_1 + nota_2
+media = soma /2
+
+
+if media >= 9.0:
+    print("sua media é", media)
+    print("sua nota é A")
+    print("APROVADO")
+elif media >= 7.5:
+    print("sua media é", media)
+    print("sua nota é B")
+    print("APROVADO")
+
+elif media >= 6.0:
+    print("sua media é", media)
+    print("sua nota é C")
+    print("APROVADO")
+
+elif media >= 4.0:
+    print("sua media é", media)
+    print("sua nota é D")
+    print("REPROVADO")
+
+else:
+    print("sua media é", media)
+    print("sua nota é F")
+    print("REPROVADO")
